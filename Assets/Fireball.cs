61e86a4f68dc17c188e9cfc65f304cd9f235c642
LitsItSchool <38472702+LitsItSchool@users.noreply.github.com>
2018-12-19 11:59:46 +0200

    Add files via upload
    up

---
+++ Assets/Fireball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+    public float speed;
+    public GameObject fireball;
+    public GameObject blast;
+    private bool isMoving;
+	// Use this for initialization
+	void Start () {
+        isMoving = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(isMoving)
+        { 
+            transform.Translate(Vector3.right*speed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fireball.SetActive(false);
+        isMoving = false;
+        blast.SetActive(true);
+        StartCoroutine(SomeDelay());
+        other.GetComponent<Rigidbody>().isKinematic = false;
+        other.GetComponent<Rigidbody>().AddRelativeForce((Vector3.right * 30+Vector3.up), ForceMode.Impulse);
+        other.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.back*100);
+        other.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.right*100);
+
+    }
+
+    IEnumerator SomeDelay()
+    {
+        yield return new WaitForSeconds(6);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/ShootGeterator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootGeterator : MonoBehaviour
+{
+    public GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+
+            var newPref = Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+    }
+}
